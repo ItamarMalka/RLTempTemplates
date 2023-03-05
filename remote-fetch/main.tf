2da4588b4efc93d6939f821d86ed758b1232985e
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-03-05 18:00:14 +0200

    Update main.tf

--- remote-fetch/main.tf
+++ remote-fetch/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "remote" {
+    hostname = "backend-dev.api.dev.env0.com"
+    organization = "9f5fbaa6-1fdd-47c1-98fb-19a4eb837723.517e576e-f235-4038-9395-c43676b1276b"
+    workspaces {
+      name = "remote-fetch-kaki"
+    }
+  }
+}
+
 resource "null_resource" "null" {
 }
 

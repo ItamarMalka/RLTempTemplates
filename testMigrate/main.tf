2486300db8a9c9133b17492dd585d47316d86d83
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-03-14 17:09:29 +0200

    Create main.tf

---
+++ testMigrate/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "remote" {
+    hostname = "backend-dev.api.dev.env0.com"
+    organization = "9f5fbaa6-1fdd-47c1-98fb-19a4eb837723"
+    workspaces {
+      name = "my-test-env"
+    }
+  }
+}
+resource "null_resource" "null" {
+}
+
+resource "null_resource" "null2" {
+}
+
+resource "null_resource" "null33" {
+}

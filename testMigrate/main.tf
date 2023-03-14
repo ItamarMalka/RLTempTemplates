2ea627e8a1ce882bdb49501229cbe326f6ff7930
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-03-14 17:17:15 +0200

    Update main.tf

--- testMigrate/main.tf
+++ testMigrate/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "backend-dev.api.dev.env0.com"
-    organization = "9f5fbaa6-1fdd-47c1-98fb-19a4eb837723"
-    workspaces {
-      name = "my-test-env"
-    }
-  }
-}
 resource "null_resource" "null" {
 }
 

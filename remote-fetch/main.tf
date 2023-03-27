3bded98eee051654685c629476f6df5feb2888bb
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-03-27 14:23:47 +0300

    Update main.tf

--- remote-fetch/main.tf
+++ remote-fetch/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "backend-pr11496.api.dev.env0.com"
+    hostname = "backend-dev.api.dev.env0.com"
     organization = "85e50daf-8893-4532-a852-e528c30f4102"
     workspaces {
       name = "remote-fetch"

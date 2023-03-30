643240cfbae9db69e72661920614d3699232dadc
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-03-30 19:19:01 +0300

    Create main.tf

---
+++ remote-fetch/d-org/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "remote" {
+    hostname = "backend-dev.api.dev.env0.com"
+    organization = "9f5fbaa6-1fdd-47c1-98fb-19a4eb837723"
+    workspaces {
+      name = "remote-fetch-d-org"
+    }
+  }
+}
+
+data "terraform_remote_state" "test" {
+ backend = "remote"
+ config = {
+   hostname     = "backend-dev.api.dev.env0.com"
+   token        = ""
+   organization = "9f5fbaa6-1fdd-47c1-98fb-19a4eb837723"
+   workspaces = {
+     name = "remote-fetch"
+   }
+ }
+}
+
+resource "null_resource" "null" {
+}
+
+output "fetched-test" {
+  value = data.terraform_remote_state.test.outputs.test
+}

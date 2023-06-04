5bb8cd341ec5116551f29816117e13a72056a86c
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-06-04 17:05:13 +0300

    Update bucket.tf

--- runallTest/bucket/bucket.tf
+++ runallTest/bucket/bucket.tf
@@ -6,10 +6,47 @@ resource "random_string" "random" {
   min_lower = 16
 }
 
-resource "null_resource" "resource" {
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "hello-env0-${random_string.random.result}"
+  acl    = "public-read"
+
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
 }
 
-output "null_resource_id" {
-  description = "The `id` of the `null_resource` resource in this module."
-  value       = "${null_resource.resource.id}"
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "Public-Access",
+  "Statement": [
+    {
+      "Sid": "Allow-Public-Access-To-Bucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": [
+          "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+output "bucket_name" {
+  value =  "${aws_s3_bucket.website_bucket.arn}"
 }

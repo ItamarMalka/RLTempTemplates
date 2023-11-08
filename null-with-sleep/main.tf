d08693c7d646b077549419dad4d72d66f9aa4bd5
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-11-08 15:11:38 +0200

    Smaller but still same size

--- null-with-sleep/main.tf
+++ null-with-sleep/main.tf
@@ -12,7 +12,7 @@ resource "null_resource" "null33" {
 }
 
 resource "random_id" "random" {
-  byte_length = 4000000
+  byte_length = 1000000
 }
 
 output "long1" {
@@ -20,5 +20,29 @@ output "long1" {
 }
 
 output "long2" {
+  value= random_id.random.hex
+}
+
+output "long3" {
+  value= random_id.random.hex
+}
+
+output "long4" {
+  value= random_id.random.hex
+}
+
+output "long5" {
+  value= random_id.random.hex
+}
+
+output "long6" {
+  value= random_id.random.hex
+}
+
+output "long7" {
+  value= random_id.random.hex
+}
+
+output "long8" {
   value= random_id.random.hex
 }

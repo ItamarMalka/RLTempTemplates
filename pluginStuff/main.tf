fee54d5c48602478230c53e83e26cf2552103ad2
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2024-03-31 15:00:34 +0300

    Update main.tf

--- pluginStuff/main.tf
+++ pluginStuff/main.tf
@@ -2,5 +2,15 @@ terraform {
   required_version = ">= 0.12.20"
 }
 
+terraform {
+  backend "remote" {
+    hostname = "backend-dev.api.dev.env0.com"
+    organization = "f816cf52-a950-4747-ac8f-9313f4f78104"
+    workspaces {
+      name = "shared-environment-757575"
+    }
+  }
+}
+
 resource "null_resource" "null" {
 }
